66abfa3831913a58d69c335300e84c976ab2d727
eduard.tobilko <1992eduard777@gmail.com>
2025-09-26 18:05:36 -0400

    home_task-2-routers

--- src/blogs/routers/blogs.router.ts
+++ src/blogs/routers/blogs.router.ts
@@ -1,14 +1,14 @@
 import { Router } from "express";
 
 import { adminGuardMiddlewareAuth } from "../../auth/admin-guard.middleware";
-import { blogParamIdMiddlewareValidation } from "../../core/middlewares/validations/blog-param-id-validation.middleware";
+import { paramIdMiddlewareValidation } from "../../core/middlewares/validations/param-id-validation.middleware";
 import { inputValidationResultMiddleware } from "../../core/middlewares/validations/input-validation-result.middleware";
 import { blogBodyInputValidationMiddleware } from "../../core/middlewares/validations/blog-body-input-validation.middleware";
-import { getBlogListHandler } from "./handlers/get-blog-list.handler";
-import { getBlogByIdHandler } from "./handlers/get-blog.handler";
-import { createNewBlogHandler } from "./handlers/create-blog.handler";
-import { deleteBlogHandler } from "./handlers/delete-blog.handler";
-import { updateBlogHandler } from "./handlers/update-blog.handler";
+import { getBlogListHandler } from "./handlers/blog-handlers/get-blog-list.handler";
+import { getBlogByIdHandler } from "./handlers/blog-handlers/get-blog.handler";
+import { createNewBlogHandler } from "./handlers/blog-handlers/create-blog.handler";
+import { deleteBlogHandler } from "./handlers/blog-handlers/delete-blog.handler";
+import { updateBlogHandler } from "./handlers/blog-handlers/update-blog.handler";
 
 export const blogsRouter = Router({});
 
@@ -16,7 +16,7 @@ blogsRouter.get("", getBlogListHandler);
 
 blogsRouter.get(
   "/:id",
-  blogParamIdMiddlewareValidation,
+  paramIdMiddlewareValidation,
   inputValidationResultMiddleware,
   getBlogByIdHandler
 );
@@ -32,7 +32,7 @@ blogsRouter.post(
 blogsRouter.put(
   "/:id",
   adminGuardMiddlewareAuth,
-  blogParamIdMiddlewareValidation,
+  paramIdMiddlewareValidation,
   blogBodyInputValidationMiddleware,
   inputValidationResultMiddleware,
   updateBlogHandler
@@ -41,7 +41,7 @@ blogsRouter.put(
 blogsRouter.delete(
   "/:id",
   adminGuardMiddlewareAuth,
-  blogParamIdMiddlewareValidation,
+  paramIdMiddlewareValidation,
   inputValidationResultMiddleware,
   deleteBlogHandler
 );
--- src/blogs/routers/posts.router.ts
+++ src/blogs/routers/posts.router.ts
@@ -1,12 +1,26 @@
-import { Request, Response, Router } from "express";
+import { Router } from "express";
 
-import { db } from "../../db/mock.db";
-import { HTTP_STATUS_CODES } from "../../core/utils/http-statuses.util";
+import { getPostListHandler } from "./handlers/post-handlers/get-post-list.handler";
+import { getPostHandler } from "./handlers/post-handlers/get-post.handler";
+import { paramIdMiddlewareValidation } from "../../core/middlewares/validations/param-id-validation.middleware";
+import { inputValidationResultMiddleware } from "../../core/middlewares/validations/input-validation-result.middleware";
+import { createNewPostHandler } from "./handlers/post-handlers/create-post.handler";
+import { adminGuardMiddlewareAuth } from "../../auth/admin-guard.middleware";
+import { postBodyInputValidationMiddleware } from "../../core/middlewares/validations/post-body-input-validation.middleware";
 
 export const postsRouter = Router({});
 
-postsRouter.get("", (_req: Request, res: Response) => {
-  const posts = db.posts;
-
-  res.status(HTTP_STATUS_CODES.OK_200).json(posts);
-});
+postsRouter.get("", getPostListHandler);
+postsRouter.get(
+  "/:id",
+  paramIdMiddlewareValidation,
+  inputValidationResultMiddleware,
+  getPostHandler
+);
+postsRouter.post(
+  "",
+  adminGuardMiddlewareAuth,
+  postBodyInputValidationMiddleware,
+  inputValidationResultMiddleware,
+  createNewPostHandler
+);
