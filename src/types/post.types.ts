7600c33acd5f35450cac75896cb3e4ed0b647ee9
eduard.tobilko <1992eduard777@gmail.com>
2025-09-26 18:03:13 -0400

    home_task-2-types

--- src/types/post.types.ts
+++ src/types/post.types.ts
@@ -1,8 +1,15 @@
 export type PostTypeModel = {
-  id: string;
+  id: string | number;
   title: string;
   shortDescription: string;
   content: string;
   blogId: string;
   blogName: string;
+};
+
+export type PostInputDtoTypeModel = {
+  title: string;
+  shortDescription: string;
+  content: string;
+  blogId: string;
 };
