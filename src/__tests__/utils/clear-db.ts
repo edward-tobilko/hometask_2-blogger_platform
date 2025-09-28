4b19841a903b321a13866836307135b11107df8c
eduard.tobilko <1992eduard777@gmail.com>
2025-09-28 17:49:28 -0400

    home_task-2-created helper func

---
+++ src/__tests__/utils/clear-db.ts
@@ -0,0 +1,13 @@
+import { Express } from "express";
+import request from "supertest";
+
+import { TESTING_PATH } from "../../core/paths/paths";
+import { HTTP_STATUS_CODES } from "../../core/utils/http-statuses.util";
+
+export async function clearDB(app: Express) {
+  await request(app)
+    .delete(TESTING_PATH)
+    .expect(HTTP_STATUS_CODES.NO_CONTENT_204);
+
+  return;
+}
