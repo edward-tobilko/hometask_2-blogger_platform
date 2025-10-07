b54ef33bff21c9268b3ff9c61945e7bc537de89b
eduard.tobilko <1992eduard777@gmail.com>
2025-10-07 15:23:01 -0400

    home_task-2

--- src/blogs/routers/blogs.router.ts
+++ src/blogs/routers/blogs.router.ts
@@ -3,7 +3,7 @@ import { Router } from "express";
 import { adminGuardMiddlewareAuth } from "../../auth/admin-guard.middleware";
 import { paramIdMiddlewareValidation } from "../../core/middlewares/validations/param-id-validation.middleware";
 import { inputValidationResultMiddleware } from "../../core/middlewares/validations/input-validation-result.middleware";
-import { blogBodyInputValidationMiddleware } from "../validations/blog-body-input-validation.middleware";
+import { blogBodyInputValidationMiddleware } from "../validations/blog-dto-input-validation.middleware";
 import { getBlogListHandler } from "./handlers/get-blog-list.handler";
 import { getBlogByIdHandler } from "./handlers/get-blog.handler";
 import { createNewBlogHandler } from "./handlers/create-blog.handler";
