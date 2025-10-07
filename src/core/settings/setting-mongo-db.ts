943049562c4783b3bceec215aeff64356ad63332
eduard.tobilko <1992eduard777@gmail.com>
2025-10-07 15:22:29 -0400

    home_task-2-fixed url path

--- src/core/settings/setting-mongo-db.ts
+++ src/core/settings/setting-mongo-db.ts
@@ -2,6 +2,6 @@ export const SETTINGS_MONGO_DB = {
   PORT: process.env.PORT || 5001,
   MONGO_URL:
     process.env.MONGO_URL ||
-    "mongodb+srv://1992eduard777_db_user:!Miami4769@hometask-2.zq4rutx.mongodb.net/?retryWrites=true&w=majority&appName=hometask-2",
+    "mongodb+srv://1992eduard777_db_user:!Miami4769@hometask-2.zq4rutx.mongodb.net",
   DB_NAME: process.env.DB_NAME || "home_task2-blogger_platform",
 };
