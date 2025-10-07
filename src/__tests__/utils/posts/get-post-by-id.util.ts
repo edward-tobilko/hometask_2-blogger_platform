ed8177ae38bf98e1d15571e697caa60e8f716f01
eduard.tobilko <1992eduard777@gmail.com>
2025-10-07 19:35:13 -0400

    home_task-2-added new utility (getPostByIdResponseCodeUtil)

--- src/__tests__/utils/posts/get-post-by-id.util.ts
+++ src/__tests__/utils/posts/get-post-by-id.util.ts
@@ -1,1 +1,24 @@
-export function getPostByIdUtil() {}
+import request from "supertest";
+import { Express } from "express";
+
+import { PostView } from "../../../posts/types/post.types";
+import { POSTS_PATH } from "../../../core/paths/paths";
+import { HTTP_STATUS_CODES } from "../../../core/utils/http-statuses.util";
+
+export async function getPostByIdBodyUtil(
+  app: Express,
+  postId: string
+): Promise<PostView> {
+  const fetchedPostById = await request(app)
+    .get(`${POSTS_PATH}/${postId}`)
+    .expect(HTTP_STATUS_CODES.OK_200);
+
+  return fetchedPostById.body;
+}
+
+export async function getPostByIdResponseCodeUtil(
+  app: Express,
+  postId: string
+): Promise<request.Response> {
+  return await request(app).get(`${POSTS_PATH}/${postId}`);
+}
