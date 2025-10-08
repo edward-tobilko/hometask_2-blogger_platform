8d39bb30fa1324e4ac989dc7bdae5f2dfeb8cfcf
eduard.tobilko <1992eduard777@gmail.com>
2025-10-08 11:13:06 -0400

    home_task-2-added .env

--- src/core/settings/setting-mongo-db.ts
+++ src/core/settings/setting-mongo-db.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const SETTINGS_MONGO_DB = {
-  PORT: process.env.PORT || 5001,
-  MONGO_URL: process.env.MONGO_URL,
-  DB_NAME: process.env.DB_NAME || "home_task2-blogger_platform",
+  PORT: Number(process.env.PORT) || 5001,
+  MONGO_URL: process.env.MONGO_URL ?? "mongodb://127.0.0.1:27017",
+  DB_NAME: process.env.DB_NAME ?? "home_task2-blogger_platform",
 };
