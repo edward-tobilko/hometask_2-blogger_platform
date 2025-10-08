b44ec9326b52503582a18f831c105dee38b63cf5
eduard.tobilko <1992eduard777@gmail.com>
2025-10-08 15:52:24 -0400

    home_task-2

--- src/server.ts
+++ src/server.ts
@@ -8,19 +8,26 @@ const bootstrap = async () => {
   const app = express();
   setupApp(app);
 
+  // * Render задаємо PORT автоматично. Локально 5001.
   const PORT = Number(SETTINGS_MONGO_DB.PORT ?? 5001);
 
-  await runDB(SETTINGS_MONGO_DB.MONGO_URL);
+  try {
+    await runDB(SETTINGS_MONGO_DB.MONGO_URL);
 
-  const server = app.listen(PORT);
+    const server = app.listen(PORT, () => {
+      console.log(`✅ Server running on http://localhost:${PORT}`);
 
-  server.on("listening -> ", () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-  });
+      server.on("error", (err) => {
+        console.error("❌ Server error:", err);
+        process.exit(1);
+      });
 
-  console.log("ENTRY:", __filename);
-
-  return app;
+      console.log("✅ ENTRY:", __filename);
+    });
+  } catch (error) {
+    console.error("❌ Failed to start app:", error);
+    process.exit(1);
+  }
 };
 
 bootstrap();
