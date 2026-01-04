0b9cf0e34542c11195da64ba35a2643accdce783
eduard.tobilko <1992eduard777@gmail.com>
2026-01-04 13:07:59 -0500

    sprint_2-week_3-homeTask_2

--- src/auth/adapters/nodemailer-service.adapter.ts
+++ src/auth/adapters/nodemailer-service.adapter.ts
@@ -9,6 +9,11 @@ const transporter = nodemailer.createTransport({
     user: appConfig.EMAIL, // нашь email
     pass: appConfig.EMAIL_PASS, // получаем сгенерированный код в настройках гугл аккаунта (https://myaccount.google.com/security )
   },
+
+  // 👇 ОЦЕ ВАЖЛИВО
+  connectionTimeout: 5_000, // встановлення TCP
+  greetingTimeout: 5_000, // очікування привітання SMTP
+  socketTimeout: 10_000, // загальний таймаут сокета
 });
 
 export const nodeMailerService = {
@@ -17,17 +22,26 @@ export const nodeMailerService = {
     code: string, // код подтверджения
     template: (code: string) => string // ф-я которая принимает код и отправляет html строку
   ): Promise<boolean> {
-    const info = await transporter.sendMail({
-      from: `"eddie" <${appConfig.EMAIL}>`,
-      to: email,
-      subject: "Your code is here",
-      html: template(code), // html body
-    });
+    // * отправку сообщения лучше обернуть в try-catch, чтобы при ошибке (например отвалиться отправка) приложение не падало
+    try {
+      log("SENDING EMAIL TO:", email);
 
-    log("info ->", info);
+      const info = await transporter.sendMail({
+        from: `"eddie" <${appConfig.EMAIL}>`,
+        to: email,
+        subject: "Your code is here",
+        html: template(code), // html body
+      });
 
-    return info.accepted.length > 0; // так будет надежней, если вдруг будет не валидный email
-    // return !!info;
+      log("SENT:", info.messageId, info.accepted, info.rejected);
+
+      return info.accepted.length > 0; // так будет надежней, если вдруг будет не валидный email
+      // return !!info;
+    } catch (error: unknown) {
+      console.error("EMAIL_SEND_ERROR", error);
+
+      return false;
+    }
   },
 };
 
