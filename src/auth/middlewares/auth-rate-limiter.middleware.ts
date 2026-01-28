d1c25c241d3b28678966a1b9872d9597b7e25457
eduard.tobilko <1992eduard777@gmail.com>
2026-01-27 21:01:02 -0500

    sprint_3-week_1-homeTask_2 - added skip method

--- src/auth/middlewares/auth-rate-limiter.middleware.ts
+++ src/auth/middlewares/auth-rate-limiter.middleware.ts
@@ -3,10 +3,15 @@ import rateLimit, { RateLimitRequestHandler } from "express-rate-limit";
 import { HTTP_STATUS_CODES } from "@core/result/types/http-status-codes.enum";
 
 export const authRateLimiter: RateLimitRequestHandler = rateLimit({
-  windowMs: 10 * 1000, // 10 сек
+  windowMs: 10 * 1000, // 10сек
   max: 5, // максимум 5 запроссов
   standardHeaders: true, // добавляет RateLimit-заголовки
   legacyHeaders: false, // не добавляет X-RateLimit-*
+
+  skip: () => process.env.NODE_ENV === "test",
+  skipFailedRequests: false,
+  skipSuccessfulRequests: false,
+
   message: {
     errorsMessages: [
       {
