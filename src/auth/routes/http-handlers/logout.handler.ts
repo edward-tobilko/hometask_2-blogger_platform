58d7654fdbdf2341339d8d59de20616a32840bdd
eduard.tobilko <1992eduard777@gmail.com>
2026-01-31 13:45:21 -0500

    sprint_3-week_1-homeTask_2 - refactoring

--- src/auth/routes/http-handlers/logout.handler.ts
+++ src/auth/routes/http-handlers/logout.handler.ts
@@ -4,6 +4,7 @@ import { errorsHandler } from "../../../core/errors/errors-handler.error";
 import { HTTP_STATUS_CODES } from "@core/result/types/http-status-codes.enum";
 import { JWTService } from "auth/adapters/jwt-service.adapter";
 import { authService } from "auth/application/session.service";
+import { sessionQueryRepo } from "auth/repositories/session-query.repo";
 
 export const logoutHandler = async (req: Request, res: Response) => {
   try {
@@ -14,6 +15,19 @@ export const logoutHandler = async (req: Request, res: Response) => {
 
     const payload = await JWTService.verifyRefreshToken(refreshToken);
     if (!payload) return res.sendStatus(HTTP_STATUS_CODES.UNAUTHORIZED_401);
+
+    const { deviceId, userId, iat } = payload;
+
+    const session = await sessionQueryRepo.findByDeviceId(deviceId);
+    if (!session) return res.sendStatus(HTTP_STATUS_CODES.UNAUTHORIZED_401);
+
+    if (
+      session.userId.toString() !== userId ||
+      session.deviceId !== deviceId ||
+      session.refreshIat !== iat
+    ) {
+      return res.sendStatus(HTTP_STATUS_CODES.UNAUTHORIZED_401);
+    }
 
     await authService.logout(payload.sessionId);
 
