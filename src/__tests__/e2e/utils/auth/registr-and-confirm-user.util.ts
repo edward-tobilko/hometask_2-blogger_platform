ea72ccd36de1ba3f595c8e108fe88e967f07b1fb
eduard.tobilko <1992eduard777@gmail.com>
2026-02-08 14:02:47 -0500

    Sprint-3 / Week-2 - refactored setRegisterAndConfirmUser func

--- src/__tests__/e2e/utils/auth/registr-and-confirm-user.util.ts
+++ src/__tests__/e2e/utils/auth/registr-and-confirm-user.util.ts
@@ -1,32 +1,53 @@
-import express from "express";
+import { IUsersQueryRepository } from "users/interfaces/IUsersQueryRepository";
+import { Express } from "express";
 
 import { getUserDto } from "../users/get-user-dto.util";
 import { createAuthRegisterUser } from "./auth-registr.util";
-import { setupApp } from "app";
 import { HTTP_STATUS_CODES } from "@core/result/types/http-status-codes.enum";
-import { userCollection } from "db/mongo.db";
 import { createAuthConfirmRegistration } from "./auth-confirm-registr.util";
+import { container } from "@core/di/inversify.config";
+import { Types } from "@core/di/types";
 
-export async function setRegisterAndConfirmUser() {
-  const app = express();
-  setupApp(app);
+type RegisterUserTestResult = {
+  email: string;
+  login: string;
+  password: string;
+};
 
+export async function setRegisterAndConfirmUser(
+  app: Express,
+  _options?: { email: string; login: string; password: string }
+): Promise<RegisterUserTestResult> {
+  // * get mock data
   const userDto = getUserDto();
+
+  const { email, login, password } = userDto;
 
   // * register
   await createAuthRegisterUser(app, userDto).expect(
     HTTP_STATUS_CODES.NO_CONTENT_204
   );
 
+  // * get confirmation code from mongo by IoC
+  const usersQueryRepo = container.get<IUsersQueryRepository>(
+    Types.IUsersQueryRepository
+  );
+
   // * take status code from db
-  const user = await userCollection.findOne({ email: userDto.email });
+  const user = await usersQueryRepo.findByEmail(email);
 
-  if (!user) throw new Error("User was not created");
+  const confirmCode = user?.emailConfirmation.confirmationCode;
 
-  // * confirm
+  if (!confirmCode) throw new Error("Email confirmation code not found in DB");
+
+  // * confirm email
   await createAuthConfirmRegistration(app, {
-    code: user.emailConfirmation.confirmationCode,
+    code: confirmCode,
   }).expect(HTTP_STATUS_CODES.NO_CONTENT_204);
 
-  return userDto;
+  return {
+    email,
+    login,
+    password,
+  };
 }
