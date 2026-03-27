1b1b74672de1aaaa9eed7bdf9f26da15d5625098
eduard.tobilko <1992eduard777@gmail.com>
2026-03-27 15:33:37 -0400

    sprint-3 / week-4 - mongoose and DDD: added  "pino" logger package and config

---
+++ src/logger.ts
@@ -0,0 +1,33 @@
+import pino from "pino";
+
+import { appConfig } from "@core/settings/config";
+
+export const log = pino({
+  transport: {
+    target: "pino-pretty",
+    options: {
+      colorize: true,
+      translateTime: "SYS:standard",
+    },
+  },
+  level: appConfig.NODE_ENV === "production" ? "info" : "debug",
+});
+
+// ? Теперь используем этот плагин, вместо console.log().
+
+// * ❌ было
+// ? console.log("ERROR HANDLER:", error);
+// ? console.log("REQ.USER SET:", req.user);
+
+// * ✅ стало
+// ? logger.error({ error }, "ERROR HANDLER");
+// ? logger.info({ user: req.user }, "REQ.USER SET");
+// ? logger.debug({ postId }, "Fetching post");
+
+// * Уровни логирования:
+// ? logger.trace("самый детальный");
+// ? logger.debug("для разработки");
+// ? logger.info("обычные события");
+// ? logger.warn("предупреждения");
+// ? logger.error("ошибки");
+// ? logger.fatal("критические ошибки");
