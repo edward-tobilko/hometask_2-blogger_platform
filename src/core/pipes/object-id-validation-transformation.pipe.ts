8854e1e24933df7ed131384453780cb2c8e63db4
eduard.tobilko <1992eduard777@gmail.com>
2026-04-29 18:03:48 -0400

    sprint-4 / week-2: migration to NestJs (validation / guards / filters / tests)

--- src/core/pipes/object-id-validation-transformation.pipe.ts
+++ src/core/pipes/object-id-validation-transformation.pipe.ts
@@ -1,4 +1,4 @@
-import { ArgumentMetadata, PipeTransform } from '@nestjs/common';
+import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
 import { isValidObjectId, Types } from 'mongoose';
 
 import {
@@ -7,7 +7,8 @@ import {
 } from '../exceptions/domain.exceptions';
 
 // * Custom pipe example (https://docs.nestjs.com/pipes#custom-pipes)
-export class IdValidationTransformationPipe implements PipeTransform {
+@Injectable()
+export class ObjectIdValidationTransformationPipe implements PipeTransform {
   transform(value: string, metadata: ArgumentMetadata) {
     // * Проверяем, что тип данных в декораторе — ObjectId.
     if (metadata.metatype !== Types.ObjectId) {
@@ -22,5 +23,24 @@ export class IdValidationTransformationPipe implements PipeTransform {
     }
 
     return new Types.ObjectId(value); // Преобразуем строку в ObjectId
+  }
+}
+
+@Injectable()
+export class IdValidationPipe implements PipeTransform {
+  transform(value: string, metadata: ArgumentMetadata) {
+    // * Проверяем, что тип данных в декораторе — string.
+    if (metadata.metatype !== String) {
+      return value; // Если тип не string, возвращаем значение без изменений
+    }
+
+    if (!isValidObjectId(value)) {
+      throw new DomainException({
+        code: DomainExceptionCode.BadRequest,
+        message: `Invalid Id: ${value}`,
+      });
+    }
+
+    return value;
   }
 }
