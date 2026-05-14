50adefd516642bdd3864fbd8c5a70e827a81f586
eduard.tobilko <1992eduard777@gmail.com>
2026-05-14 17:55:43 -0400

    sprint-4 / week-2: migration to NestJs (validation / guards / filters / tests)

--- src/config/throttler.module.ts
+++ src/config/throttler.module.ts
@@ -1,10 +1,18 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ThrottlerModule } from '@nestjs/throttler';
 
-export const throttlerModule = ThrottlerModule.forRoot({
-  throttlers: [
-    {
-      ttl: 10000,
-      limit: 5,
-    },
-  ],
+export const throttlerModule = ThrottlerModule.forRootAsync({
+  imports: [ConfigModule],
+  inject: [ConfigService],
+
+  useFactory: (config: ConfigService) => {
+    const isDisabled = config.get('DISABLE_RATE_LIMIT') === 'true';
+
+    return [
+      {
+        ttl: Number(config.get('TTL_RATE_LIMIT')),
+        limit: isDisabled ? 10000 : Number(config.get('COUNT_RATE_LIMIT')),
+      },
+    ];
+  },
 });
