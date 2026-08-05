1ba953f714ddca5d508e5938c03cb83171a7ca69
eduard.tobilko <1992eduard777@gmail.com>
2026-08-05 18:05:54 -0400

    Extra logic over the basic API: feature(user-accounts): check if a user is banned or unbanned

--- src/modules/user-accounts/guards/bearer/jwt.strategy.ts
+++ src/modules/user-accounts/guards/bearer/jwt.strategy.ts
@@ -3,10 +3,16 @@ import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 
 import { UserAccountsConfig } from '../../config/user-accounts.config';
+import { UsersRepository } from '../../infrastructure/repositories/users.repository';
+import { DomainException } from 'src/core/exceptions/domain.exception';
+import { DomainExceptionCode } from 'src/core/exceptions/domain.exception-codes';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-  constructor(userAccountConfig: UserAccountsConfig) {
+  constructor(
+    private userAccountConfig: UserAccountsConfig,
+    private usersRepo: UsersRepository,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
@@ -14,7 +20,17 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
     });
   }
 
-  validate(payload: { userId: string }): { id: string } {
+  async validate(payload: { userId: string }): Promise<{ id: string }> {
+    const user = await this.usersRepo.findById(payload.userId);
+
+    // * Проверка юзера на ban / unban
+    if (user?.banInfo.isBanned) {
+      throw new DomainException({
+        code: DomainExceptionCode.Unauthorized,
+        message: `Your account is permanently banned`,
+      });
+    }
+
     return { id: payload.userId };
   }
 }
